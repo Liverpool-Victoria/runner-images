af1874c13cc6b677a39ad6a8079962629bc29f8d
Andy Wilde <you@example.com>
2025-10-16 10:46:44 +0000

    install docker and rover image only

--- images/ubuntu/scripts/build/install-docker.sh
+++ images/ubuntu/scripts/build/install-docker.sh
@@ -64,6 +64,8 @@ systemctl is-enabled --quiet docker.service || systemctl enable docker.service
 sleep 10
 docker info
 
+echo DOCKERHUB_PULL_IMAGES="${DOCKERHUB_PULL_IMAGES}"
+
 if [[ "${DOCKERHUB_PULL_IMAGES:-yes}" == "yes" ]]; then
     # If credentials are provided, attempt to log into Docker Hub
     # with a paid account to avoid Docker Hub's rate limit.
